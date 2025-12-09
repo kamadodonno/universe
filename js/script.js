be0839c521e85cb675e668dabfd45088afc82e95
kamadodonno <mehulnikam22@gmail.com>
2025-12-09 20:29:06 +0530

    copying

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function nextPage() {
+  alert("This will go to the next page — like a memory timeline, love letter, etc.");
+  // You can replace alert with window.location.href = "page2.html";
+}
+
